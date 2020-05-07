a2b89843defaf31cfcfefbecb1a139d3bae5b172
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-05-07 16:20:41 +0700

    init device-disconnect

---
+++ src/app/modules/dashboard/components/widget-device/device-disconnect/device-disconnect.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-disconnect',
+  templateUrl: './device-disconnect.component.html',
+  styleUrls: ['./device-disconnect.component.scss'],
+})
+export class DeviceDisconnectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
-
 import { DashboardPage } from './dashboard.page';
 import { WidgetDeviceComponent } from './components/widget-device/widget-device.component';
+import { DeviceDisconnectComponent } from './components/widget-device/device-disconnect/device-disconnect.component';
 
 @NgModule({
   imports: [
@@ -18,7 +17,8 @@ import { WidgetDeviceComponent } from './components/widget-device/widget-device.
   ],
   declarations: [
     DashboardPage,
-    WidgetDeviceComponent
+    WidgetDeviceComponent,
+    DeviceDisconnectComponent
   ]
 })
 export class DashboardPageModule { }
